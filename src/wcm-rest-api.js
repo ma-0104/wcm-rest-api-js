64b958a93ddf7d9e4d4916745304a48dd32c0d17
Miguel Rosales <miguel.rosales@base22.com>
2020-05-11 14:37:51 -0500

    Create new WCMRestAPI class, this uses fetch api.

---
+++ src/wcm-rest-api.js
@@ -0,0 +1,27 @@
+(function() {
+	window.WCMRestAPI = class WCMRestAPI {
+
+		static makeAjaxCall( params ) {
+			const requestParams = {
+				method: params.method || "GET",
+				cache: "no-cache",
+				credentials: "same-origin",
+				headers: {
+					"Content-Type": params.contentType || "application/json",
+					"Accept": params.contentType || "application/json"
+				},
+				referrerPolicy: "no-referrer"
+			};
+
+			if( params.body ) requestParams.body = JSON.stringify( params.body );
+
+			const fetchCall = fetch( params.url, requestParams );
+			return new Promise( ( resolve, reject ) => {
+				fetchCall.then( response => response.json() ).then( data => resolve( data ) );
+			} )
+		};
+
+	}
+
+})
+();
