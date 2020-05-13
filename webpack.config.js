5aeecc02c32f1fe0c3d15b3e6c536d7ff0897fff
Alejandro Serrano <alexserrano63@gmail.com>
2020-05-12 20:20:13 -0400

    Add webpack configuration: WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: ["@babel/polyfill", "./src/"],
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, './build'),
