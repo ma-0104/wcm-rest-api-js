c3cb9f6d9ffcb587e82af59dca49f32f2b2f569a
Alejandro Serrano <alexserrano63@gmail.com>
2020-05-13 00:50:48 -0400

    Update wcm-rest-api.js

--- src/utils/wcm-rest-api.js
+++ src/utils/wcm-rest-api.js
@@ -1,37 +1,32 @@
 class WCMRestAPI {
+    constructor(virtualPortal) {
+        this.virtualPortal = virtualPortal ? `${virtualPortal}/` : "";
+        this.wcmRestContext = `/wps/contenthandler/${this.virtualPortal}!ut/p/wcmrest/`;
+    }
 
+    static makeAjaxCall(params) {
+        const requestParams = {
+            method: params.method || "GET",
+            cache: "no-cache",
+            credentials: params.credentials || "same-origin",
+            headers: {
+                "Content-Type": params.contentType || "application/json",
+                "Accept": params.contentType || "application/json"
+            },
+            referrerPolicy: "no-referrer"
+        };
+
+        if (params.body) requestParams.body = JSON.stringify(params.body);
+
+        const fetchCall = fetch(params.url, requestParams);
+        return new Promise((resolve, reject) => {
+            fetchCall
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        })
+    };
 }
-export { WCMRestAPI };
-/*
-export const restAPI = (function() {
-	window.WCMRestAPI = class WCMRestAPI {
 
-		constructor( virtualPortal ) {
-			this.virtualPortal = virtualPortal ? `${virtualPortal}/` : "";
-			this.wcmRestContext = `/wps/contenthandler/${this.virtualPortal}!ut/p/wcmrest/`;
-		}
+export {WCMRestAPI};
 
-		static makeAjaxCall( params ) {
-			const requestParams = {
-				method: params.method || "GET",
-				cache: "no-cache",
-				credentials: "same-origin",
-				headers: {
-					"Content-Type": params.contentType || "application/json",
-					"Accept": params.contentType || "application/json"
-				},
-				referrerPolicy: "no-referrer"
-			};
-
-			if( params.body ) requestParams.body = JSON.stringify( params.body );
-
-			const fetchCall = fetch( params.url, requestParams );
-			return new Promise( ( resolve, reject ) => {
-				fetchCall.then( response => response.json() ).then( data => resolve( data ) );
-			} )
-		};
-
-	}
-
-})
-();*/
